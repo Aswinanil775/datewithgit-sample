f1124a6a1505a95d42c776ce2f3c9aedd2c336e4
Aswinanil775 <aswinanil775@gmail.com>
2021-05-28 19:21:27 +0530

    My project

---
+++ src/Sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sum {
+public static void main(String ar[]) {
+	System.out.println("Enter two number");
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	int b=s.nextInt();
+	int c=a+b;
+	System.out.println ("Result:"+c);
+}
+}
